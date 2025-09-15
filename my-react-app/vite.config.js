383ab5e11d7894026ec2f836e03ad141b8acadf8
requiem2ks <kolzovivan812@mail.ru>
2025-09-16 02:26:29 +0300

    added 404.html and configured base for pages

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/web-application/',
   plugins: [react()],
   server: {
     historyApiFallback: true, // ← ЭТО РЕШАЕТ ПРОБЛЕМУ!
